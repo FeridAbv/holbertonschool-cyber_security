0d896600dc93fe3f4581806c84126a66953f118b
FeridAbv <feridabv6577@gmail.com>
2026-08-14 15:53:52 -0400

    ae

--- web_application_security/0x0c_web_application_foresics/4-firewall.sh
+++ web_application_security/0x0c_web_application_foresics/4-firewall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Scan auth.log and count how many firewall rules were added
-# to the system (iptables -A / -I or ufw allow / deny / insert).
+# to the system using iptables (the -A flag appends a new rule).
 # Usage: ./4-firewall.sh [logfile]
 # If no logfile is given, ./auth.log is used by default.
 
@@ -11,4 +11,4 @@ else
 	logfile=$1
 fi
 
-grep -iE "iptables.*(-A|-I)|ufw.*(allow|deny|insert|reject)" $logfile | wc -l
+grep "iptables" $logfile | grep -c -- "-A"
